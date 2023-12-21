d4cdd26be2d5548a8dea7bef5261c6833c0ad1a0
mealibek <mealibek@hotmail.com>
2023-12-22 00:21:39 +0600

    bla bla bla

--- main.go
+++ main.go
@@ -6,33 +6,29 @@ import (
 	"time"
 )
 
-func dowork(d time.Duration, s string, ch chan string) {
-	fmt.Println("Doing work....")
+func dowork(d time.Duration, s string, ch chan string, wg *sync.WaitGroup) {
+	defer wg.Done() // decrements groups by one
+	fmt.Println("Doing work....", s)
 	time.Sleep(d)
-	fmt.Println("Work is done...")
+	fmt.Println("Work is done...", s)
 	ch <- s
-	wg.Done() // decrements groups by one--;
 }
-
-var wg *sync.WaitGroup
 
 func main() {
 	start := time.Now()
-	ch := make(chan string)
-	wg = &sync.WaitGroup{}
+	ch := make(chan string, 3) // Create a buffered channel with a buffer size of 3
+	wg := &sync.WaitGroup{}
 	wg.Add(3)
-	go dowork(time.Second*2, "work 1", ch)
-	go dowork(time.Second*4, "work 2", ch)
-	go dowork(time.Second*6, "work 3", ch)
 
-	go func() {
-		for res := range ch {
-			fmt.Println(res)
-		}
-		close(ch)
-	}()
+	go dowork(time.Second*2, "work 1", ch, wg)
+	go dowork(time.Second*4, "work 2", ch, wg)
+	go dowork(time.Second*6, "work 3", ch, wg)
 
-	wg.Wait() // waits untill groups are finished...
+	wg.Wait() // waits until groups are finished...
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 
 	fmt.Printf("Work took %v seconds\n", time.Since(start))
 	time.Sleep(time.Second)
